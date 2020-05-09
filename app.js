b83ed06100c5a1cdb7cf4adb7d241c0fd8ab8813
PardeepBaboria <pardeepbaboria@gmail.com>
2020-05-09 19:54:54 +0530

    add cors lets see.

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const path = require("path");
 const express = require("express");
+const cors = require("cors");
 // const viewRouter = require('./routes/viewRoutes');
 
 // Start express app
 const app = express();
+
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, front-end natours.com
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }))
+
+app.options("*", cors());
+// app.options('/api/v1/tours/:id', cors());
 
 app.use(express.static(path.join(__dirname, "public")));
 
